240bb85a18ad53b3887908d00cef4793c12c9848
gilmae <avocadia@fastmail.fm>
2021-08-28 18:33:13 +1000

    First draft of a cli

---
+++ mf.cli/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+
+namespace mf.cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url;
+            mf.Parser p = new mf.Parser();
+            if (args.Length > 0)
+            {
+                url = args[0];
+                var doc = p.Parse(new Uri(url));
+                Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(doc, new JsonSerializerOptions { WriteIndented = true }));
+
+            }
+        }
+    }
+}
